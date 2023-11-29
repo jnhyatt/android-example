63c246835d0eb545e3bf07190d717ff192dee4c9
Josh Hyatt <joshua.n.hyatt@gmail.com>
2023-11-29 10:30:13 -0700

    Upgrade gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    kotlin("android") version "1.9.20" apply false
+}
+
+//buildscript {
+//    ext.kotlin_version = "1.6.10"
+//    repositories {
+//        google()
+//        mavenCentral()
+//    }
+//    dependencies {
+//        classpath 'com.android.tools.build:gradle:4.2.2'
+//        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+//    }
+//}
+
+//allprojects {
+//    repositories {
+//        google()
+//        maven { url "https://jitpack.io" }
+//        mavenCentral()
+//    }
+//}
